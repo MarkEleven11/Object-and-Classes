b4d6d3f5e2fd7a9898677feaf13197190c4d9f0d
Александр <alexakr1993@gmail.com>
2022-11-28 10:49:27 +0400

    Done

--- src/Author.java
+++ src/Author.java
@@ -1,17 +1,17 @@
 public class Author {
- private String name;
- private String surname;
+    private String name;
+    private String surname;
 
- public Author(String name, String surname) {
- this.name = name;
- this.surname = surname;
- }
+     Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 
- public String getName() {
-  return name;
- }
+    public String getName() {
+        return name;
+    }
 
- public String getSurname() {
-  return surname;
- }
+    public String getSurname() {
+        return surname;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        Book theWatchmen = new Book("The Watchmen", 1987);
-        Book greatestSeller = new Book("The World's Greatest Seller", 2002);
 
         Author moore = new Author("Alan", "Moore");
+        Book theWatchmen = new Book("The Watchmen", 1987);
+
         Author og = new Author("Og", "Mandino");
+        Book greatestSeller = new Book("The World's Greatest Seller", 2002);
 
         System.out.println(theWatchmen.getTitleBook() + ", " + moore.getName() + " " + moore.getSurname() + ", " + theWatchmen.getYearPublisher() + ".");
         System.out.println(greatestSeller.getTitleBook() + ", " + og.getName() + " " + og.getSurname() + ", " + greatestSeller.getYearPublisher() + ".");
